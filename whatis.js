78b876e69d842066ab9cab32397044e53c91e85d
Juha Tauriainen <juha.tauriainen@houston-inc.com>
2018-07-29 11:08:08 +0300

    Add prettier

--- whatis.js
+++ whatis.js
@@ -1,179 +1,198 @@
-const idunno = ({ list, resultIn }) => list.reduce((initial, key) => [...initial, key, `${key}s`], []).reduce(
-  (initial, key) => ({
-    ...initial,
-    [key]: () => ({ in: resultIn(key) }),
-  }),
-  {},
-);
+const createMethodList = ({ list, resultsIn }) =>
+  list.reduce((initial, key) => [...initial, key, `${key}s`], []).reduce(
+    (initial, key) => ({
+      ...initial,
+      [key]: () => ({ in: resultsIn(key) }),
+    }),
+    {},
+  )
 
-const createResultMethods = ({ list, methods }) => valueFrom => list.reduce(
-  (initial, key) => ({
-    ...initial,
-    [key]: methods[key](valueFrom),
-    [`${key}s`]: methods[key](valueFrom),
-  }),
-  {},
-);
+const createResultMethods = ({ list, methods }) => valueFrom =>
+  list.reduce(
+    (initial, key) => ({
+      ...initial,
+      [key]: methods[key](valueFrom),
+      [`${key}s`]: methods[key](valueFrom),
+    }),
+    {},
+  )
 
-const inTime = (val) => {
-  const time = ['second', 'minute', 'hour', 'day', 'week', 'month', 'year', 'decade'];
+const inTime = val => {
+  const time = [
+    'second',
+    'minute',
+    'hour',
+    'day',
+    'week',
+    'month',
+    'year',
+    'decade',
+  ]
 
-  const daysInYear = 365;
-  const monthsInYear = 12;
+  const daysInYear = 365
+  const monthsInYear = 12
 
-  const daysInMonth = daysInYear / monthsInYear;
+  const daysInMonth = daysInYear / monthsInYear
 
-  const secondsMethod = valueFrom => () => ({
-    second: val,
-    seconds: val,
-    minute: val * 60,
-    minutes: val * 60,
-    hour: val * 60 * 60,
-    hours: val * 60 * 60,
-    day: val * 60 * 60 * 24,
-    days: val * 60 * 60 * 24,
-    week: val * 60 * 60 * 24 * 7,
-    weeks: val * 60 * 60 * 24 * 7,
-    month: val * 60 * 60 * 24 * daysInMonth,
-    months: val * 60 * 60 * 24 * daysInMonth,
-    year: val * 60 * 60 * 24 * daysInYear,
-    years: val * 60 * 60 * 24 * daysInYear,
-    decade: val * 60 * 60 * 24 * daysInYear * 10,
-    decades: val * 60 * 60 * 24 * daysInYear * 10,
-  }[valueFrom]);
+  const secondsMethod = valueFrom => () =>
+    ({
+      second: val,
+      seconds: val,
+      minute: val * 60,
+      minutes: val * 60,
+      hour: val * 60 * 60,
+      hours: val * 60 * 60,
+      day: val * 60 * 60 * 24,
+      days: val * 60 * 60 * 24,
+      week: val * 60 * 60 * 24 * 7,
+      weeks: val * 60 * 60 * 24 * 7,
+      month: val * 60 * 60 * 24 * daysInMonth,
+      months: val * 60 * 60 * 24 * daysInMonth,
+      year: val * 60 * 60 * 24 * daysInYear,
+      years: val * 60 * 60 * 24 * daysInYear,
+      decade: val * 60 * 60 * 24 * daysInYear * 10,
+      decades: val * 60 * 60 * 24 * daysInYear * 10,
+    }[valueFrom])
 
-  const minutesMethod = valueFrom => () => ({
-    second: val / 60,
-    seconds: val / 60,
-    minute: val,
-    minutes: val,
-    hour: val * 60 * 60,
-    hours: val * 60 * 60,
-    day: val * 60 * 60 * 24,
-    days: val * 60 * 60 * 24,
-    week: val * 60 * 60 * 24 * 7,
-    weeks: val * 60 * 60 * 24 * 7,
-    month: val * 60 * 60 * 24 * daysInMonth,
-    months: val * 60 * 60 * 24 * daysInMonth,
-    year: val * 60 * 60 * 24 * daysInYear,
-    years: val * 60 * 60 * 24 * daysInYear,
-    decade: val * 60 * 60 * 24 * daysInYear * 10,
-    decades: val * 60 * 60 * 24 * daysInYear * 10,
-  }[valueFrom]);
+  const minutesMethod = valueFrom => () =>
+    ({
+      second: val / 60,
+      seconds: val / 60,
+      minute: val,
+      minutes: val,
+      hour: val * 60 * 60,
+      hours: val * 60 * 60,
+      day: val * 60 * 60 * 24,
+      days: val * 60 * 60 * 24,
+      week: val * 60 * 60 * 24 * 7,
+      weeks: val * 60 * 60 * 24 * 7,
+      month: val * 60 * 60 * 24 * daysInMonth,
+      months: val * 60 * 60 * 24 * daysInMonth,
+      year: val * 60 * 60 * 24 * daysInYear,
+      years: val * 60 * 60 * 24 * daysInYear,
+      decade: val * 60 * 60 * 24 * daysInYear * 10,
+      decades: val * 60 * 60 * 24 * daysInYear * 10,
+    }[valueFrom])
 
-  const hoursMethod = valueFrom => () => ({
-    second: val / 60 / 60,
-    seconds: val / 60 / 60,
-    minute: val / 60,
-    minutes: val / 60,
-    hour: val,
-    hours: val,
-    day: val * 24,
-    days: val * 24,
-    week: val * 24 * 7,
-    weeks: val * 24 * 7,
-    month: val * 24 * daysInMonth,
-    months: val * 24 * daysInMonth,
-    year: val * 24 * daysInYear,
-    years: val * 24 * daysInYear,
-    decade: val * 24 * daysInYear * 10,
-    decades: val * 24 * daysInYear * 10,
-  }[valueFrom]);
+  const hoursMethod = valueFrom => () =>
+    ({
+      second: val / 60 / 60,
+      seconds: val / 60 / 60,
+      minute: val / 60,
+      minutes: val / 60,
+      hour: val,
+      hours: val,
+      day: val * 24,
+      days: val * 24,
+      week: val * 24 * 7,
+      weeks: val * 24 * 7,
+      month: val * 24 * daysInMonth,
+      months: val * 24 * daysInMonth,
+      year: val * 24 * daysInYear,
+      years: val * 24 * daysInYear,
+      decade: val * 24 * daysInYear * 10,
+      decades: val * 24 * daysInYear * 10,
+    }[valueFrom])
 
-  const daysMethod = valueFrom => () => ({
-    second: val / 24 / 60 / 60,
-    seconds: val / 24 / 60 / 60,
-    minute: val / 24 / 60,
-    minutes: val / 24 / 60,
-    hour: val / 24,
-    hours: val / 24,
-    day: val,
-    days: val,
-    week: val * 7,
-    weeks: val * 7,
-    month: val * daysInMonth,
-    months: val * daysInMonth,
-    year: val * daysInYear,
-    years: val * daysInYear,
-    decade: val * daysInYear * 10,
-    decades: val * daysInYear * 10,
-  }[valueFrom]);
+  const daysMethod = valueFrom => () =>
+    ({
+      second: val / 24 / 60 / 60,
+      seconds: val / 24 / 60 / 60,
+      minute: val / 24 / 60,
+      minutes: val / 24 / 60,
+      hour: val / 24,
+      hours: val / 24,
+      day: val,
+      days: val,
+      week: val * 7,
+      weeks: val * 7,
+      month: val * daysInMonth,
+      months: val * daysInMonth,
+      year: val * daysInYear,
+      years: val * daysInYear,
+      decade: val * daysInYear * 10,
+      decades: val * daysInYear * 10,
+    }[valueFrom])
 
-  const weeksMethod = valueFrom => () => ({
-    second: val / 7 / 24 / 60 / 60,
-    seconds: val / 7 / 24 / 60 / 60,
-    minute: val / 7 / 24 / 60,
-    minutes: val / 7 / 24 / 60,
-    hour: val / 7 / 24,
-    hours: val / 7 / 24,
-    day: val / 7,
-    days: val / 7,
-    week: val,
-    weeks: val,
-    month: val * daysInMonth,
-    months: val * daysInMonth,
-    year: val * daysInYear,
-    years: val * daysInYear,
-    decade: val * daysInYear * 10,
-    decades: val * daysInYear * 10,
-  }[valueFrom]);
+  const weeksMethod = valueFrom => () =>
+    ({
+      second: val / 7 / 24 / 60 / 60,
+      seconds: val / 7 / 24 / 60 / 60,
+      minute: val / 7 / 24 / 60,
+      minutes: val / 7 / 24 / 60,
+      hour: val / 7 / 24,
+      hours: val / 7 / 24,
+      day: val / 7,
+      days: val / 7,
+      week: val,
+      weeks: val,
+      month: val * daysInMonth,
+      months: val * daysInMonth,
+      year: val * daysInYear,
+      years: val * daysInYear,
+      decade: val * daysInYear * 10,
+      decades: val * daysInYear * 10,
+    }[valueFrom])
 
-  const monthsMethod = valueFrom => () => ({
-    second: val / daysInMonth / 24 / 3600,
-    seconds: val / daysInMonth / 24 / 3600,
-    minute: val / daysInMonth / 24 / 60,
-    minutes: val / daysInMonth / 24 / 60,
-    hour: val / daysInMonth / 24,
-    hours: val / daysInMonth / 24,
-    day: val / daysInMonth,
-    days: val / daysInMonth,
-    week: val / 7,
-    weeks: val / 7,
-    month: val,
-    months: val,
-    year: val * monthsInYear,
-    years: val * monthsInYear,
-    decade: val * monthsInYear * 10,
-    decades: val * monthsInYear * 10,
-  }[valueFrom]);
+  const monthsMethod = valueFrom => () =>
+    ({
+      second: val / daysInMonth / 24 / 3600,
+      seconds: val / daysInMonth / 24 / 3600,
+      minute: val / daysInMonth / 24 / 60,
+      minutes: val / daysInMonth / 24 / 60,
+      hour: val / daysInMonth / 24,
+      hours: val / daysInMonth / 24,
+      day: val / daysInMonth,
+      days: val / daysInMonth,
+      week: val / 7,
+      weeks: val / 7,
+      month: val,
+      months: val,
+      year: val * monthsInYear,
+      years: val * monthsInYear,
+      decade: val * monthsInYear * 10,
+      decades: val * monthsInYear * 10,
+    }[valueFrom])
 
-  const yearsMethod = valueFrom => () => ({
-    second: val / daysInYear / 24 / 60 / 60,
-    seconds: val / daysInYear / 24 / 60 / 60,
-    minute: val / daysInYear / 24 / 60,
-    minutes: val / daysInYear / 24 / 60,
-    hour: val / daysInYear / 24,
-    hours: val / daysInYear / 24,
-    day: val / daysInYear,
-    days: val / daysInYear,
-    week: val / 52,
-    weeks: val / 52,
-    month: val / monthsInYear,
-    months: val / monthsInYear,
-    year: val,
-    years: val,
-    decade: val * 10,
-    decades: val * 10,
-  }[valueFrom]);
+  const yearsMethod = valueFrom => () =>
+    ({
+      second: val / daysInYear / 24 / 60 / 60,
+      seconds: val / daysInYear / 24 / 60 / 60,
+      minute: val / daysInYear / 24 / 60,
+      minutes: val / daysInYear / 24 / 60,
+      hour: val / daysInYear / 24,
+      hours: val / daysInYear / 24,
+      day: val / daysInYear,
+      days: val / daysInYear,
+      week: val / 52,
+      weeks: val / 52,
+      month: val / monthsInYear,
+      months: val / monthsInYear,
+      year: val,
+      years: val,
+      decade: val * 10,
+      decades: val * 10,
+    }[valueFrom])
 
-  const decadesMethod = valueFrom => () => ({
-    second: val / daysInYear / 24 / 60 / 60 / 10,
-    seconds: val / daysInYear / 24 / 60 / 60 / 10,
-    minute: val / daysInYear / 24 / 60 / 10,
-    minutes: val / daysInYear / 24 / 60 / 10,
-    hour: val / daysInYear / 24 / 10,
-    hours: val / daysInYear / 24 / 10,
-    day: val / daysInYear / 10,
-    days: val / daysInYear / 10,
-    week: val / 52 / 10,
-    weeks: val / 52 / 10,
-    month: val / monthsInYear / 10,
-    months: val / monthsInYear / 10,
-    year: val / 10,
-    years: val / 10,
-    decade: val,
-    decades: val,
-  }[valueFrom]);
+  const decadesMethod = valueFrom => () =>
+    ({
+      second: val / daysInYear / 24 / 60 / 60 / 10,
+      seconds: val / daysInYear / 24 / 60 / 60 / 10,
+      minute: val / daysInYear / 24 / 60 / 10,
+      minutes: val / daysInYear / 24 / 60 / 10,
+      hour: val / daysInYear / 24 / 10,
+      hours: val / daysInYear / 24 / 10,
+      day: val / daysInYear / 10,
+      days: val / daysInYear / 10,
+      week: val / 52 / 10,
+      weeks: val / 52 / 10,
+      month: val / monthsInYear / 10,
+      months: val / monthsInYear / 10,
+      year: val / 10,
+      years: val / 10,
+      decade: val,
+      decades: val,
+    }[valueFrom])
 
   const timeMethods = {
     second: secondsMethod,
@@ -184,40 +203,51 @@ const inTime = (val) => {
     month: monthsMethod,
     year: yearsMethod,
     decade: decadesMethod,
-  };
+  }
 
-  const resultInTime = createResultMethods({ list: time, methods: timeMethods });
+  const resultsInTime = createResultMethods({
+    list: time,
+    methods: timeMethods,
+  })
 
-  return idunno({ list: time, resultIn: resultInTime });
-};
+  return createMethodList({ list: time, resultsIn: resultsInTime })
+}
 
-const inWeight = (val) => {
-  const weights = ['gram', 'kilogram'];
+const inWeight = val => {
+  const weights = ['gram', 'kilogram']
 
-  const gramsMethod = valueFrom => () => ({
-    gram: val,
-    grams: val,
-    kilogram: val * 1000,
-    kilograms: val * 1000,
-  }[valueFrom]);
+  const gramsMethod = valueFrom => () =>
+    ({
+      gram: val,
+      grams: val,
+      kilogram: val * 1000,
+      kilograms: val * 1000,
+    }[valueFrom])
 
-  const kilogramsMethod = valueFrom => () => ({
-    gram: val / 1000,
-    grams: val / 1000,
-    kilogram: val,
-    kilograms: val,
-  }[valueFrom]);
+  const kilogramsMethod = valueFrom => () =>
+    ({
+      gram: val / 1000,
+      grams: val / 1000,
+      kilogram: val,
+      kilograms: val,
+    }[valueFrom])
 
   const weightMethods = {
     gram: gramsMethod,
     kilogram: kilogramsMethod,
-  };
+  }
 
-  const resultInWeight = createResultMethods({ list: weights, methods: weightMethods });
+  const resultsInWeight = createResultMethods({
+    list: weights,
+    methods: weightMethods,
+  })
 
-  return idunno({ list: weights, resultIn: resultInWeight });
-};
+  return createMethodList({
+    list: weights,
+    resultsIn: resultsInWeight,
+  })
+}
 
-const whatis = val => ({ ...inTime(val), ...inWeight(val) });
+const whatis = val => ({ ...inTime(val), ...inWeight(val) })
 
-export default whatis;
+export default whatis
--- whatis.test.js
+++ whatis.test.js
@@ -1,22 +1,25 @@
-import whatis from './whatis';
+import whatis from './whatis'
 
-const shouldHaveMethods = methods => methods.forEach((method) => {
-  describe(method, () => {
-    const base = whatis(1)[method]();
+const shouldHaveMethods = methods =>
+  methods.forEach(method => {
+    describe(method, () => {
+      const base = whatis(1)[method]()
 
-    it('should have method', () => {
-      expect(base).toBeDefined();
-      expect(base.in[method]()).toBeDefined();
-      expect(base.in[method]()).toBe(1);
-    });
+      it('should have method', () => {
+        expect(base).toBeDefined()
+        expect(base.in[method]()).toBeDefined()
+        expect(base.in[method]()).toBe(1)
+      })
 
-    describe('inner methods', () => {
-      methods.map(inMethod => it(`should have inner methods: ${inMethod}`, () => {
-        expect(base.in[inMethod]()).toBeDefined();
-      }));
-    });
-  });
-});
+      describe('inner methods', () => {
+        methods.map(inMethod =>
+          it(`should have inner method: ${inMethod}`, () => {
+            expect(base.in[inMethod]()).toBeDefined()
+          }),
+        )
+      })
+    })
+  })
 
 describe('Whatis', () => {
   describe('time units', () => {
@@ -38,10 +41,10 @@ describe('Whatis', () => {
         'years',
         'decade',
         'decades',
-      ];
+      ]
 
-      shouldHaveMethods(methods);
-    });
+      shouldHaveMethods(methods)
+    })
 
     describe('seconds', () => {
       it('should give correct seconds for 1 second', () => {
@@ -49,49 +52,49 @@ describe('Whatis', () => {
           whatis(1)
             .seconds()
             .in.seconds(),
-        ).toBe(1);
-      });
+        ).toBe(1)
+      })
 
       it('should give correct hours for 3600 second', () => {
         expect(
           whatis(3600)
             .seconds()
             .in.hours(),
-        ).toBe(1);
-      });
+        ).toBe(1)
+      })
 
       it('should give correct days for 86400 seconds', () => {
         expect(
           whatis(86400)
             .seconds()
             .in.days(),
-        ).toBe(1);
-      });
+        ).toBe(1)
+      })
 
       it('should give correct months for 86400 seconds', () => {
         expect(
           whatis(86400)
             .seconds()
             .in.months(),
-        ).toBe(0.03287671232876712);
-      });
+        ).toBe(0.03287671232876712)
+      })
 
       it('should give correct years for 86400 seconds', () => {
         expect(
           whatis(86400)
             .seconds()
             .in.years(),
-        ).toBe(0.0027397260273972607);
-      });
+        ).toBe(0.0027397260273972607)
+      })
 
       it('should give correct decades for 864000 seconds', () => {
         expect(
           whatis(864000)
             .seconds()
             .in.decades(),
-        ).toBe(0.0027397260273972603);
-      });
-    });
+        ).toBe(0.0027397260273972603)
+      })
+    })
 
     describe('hours', () => {
       it('should give correct seconds for 1 hour', () => {
@@ -99,49 +102,49 @@ describe('Whatis', () => {
           whatis(1)
             .hours()
             .in.seconds(),
-        ).toBe(3600);
-      });
+        ).toBe(3600)
+      })
 
       it('should give correct hours for 1 hour', () => {
         expect(
           whatis(1)
             .hours()
             .in.hours(),
-        ).toBe(1);
-      });
+        ).toBe(1)
+      })
 
       it('should give correct days for 24 hours', () => {
         expect(
           whatis(24)
             .hours()
             .in.days(),
-        ).toBe(1);
-      });
+        ).toBe(1)
+      })
 
       it('should give correct months for 100 hours', () => {
         expect(
           whatis(100)
             .hours()
             .in.months(),
-        ).toBe(0.136986301369863);
-      });
+        ).toBe(0.136986301369863)
+      })
 
       it('should give correct years for 100 hours', () => {
         expect(
           whatis(100)
             .hours()
             .in.years(),
-        ).toBe(0.01141552511415525);
-      });
+        ).toBe(0.01141552511415525)
+      })
 
       it('should give correct years for 1000 hours', () => {
         expect(
           whatis(1000)
             .hours()
             .in.decades(),
-        ).toBe(0.01141552511415525);
-      });
-    });
+        ).toBe(0.01141552511415525)
+      })
+    })
 
     describe('days', () => {
       it('should give correct seconds for 1 day', () => {
@@ -149,49 +152,49 @@ describe('Whatis', () => {
           whatis(1)
             .days()
             .in.seconds(),
-        ).toBe(86400);
-      });
+        ).toBe(86400)
+      })
 
       it('should give correct hours for 1 day', () => {
         expect(
           whatis(1)
             .days()
             .in.hours(),
-        ).toBe(24);
-      });
+        ).toBe(24)
+      })
 
       it('should give correct days for 365 days', () => {
         expect(
           whatis(365)
             .days()
             .in.days(),
-        ).toBe(365);
-      });
+        ).toBe(365)
+      })
 
       it('should give correct months for 30 days', () => {
         expect(
           whatis(30)
             .days()
             .in.months(),
-        ).toBe(0.9863013698630136);
-      });
+        ).toBe(0.9863013698630136)
+      })
 
       it('should give correct years for 365 days', () => {
         expect(
           whatis(365)
             .days()
             .in.years(),
-        ).toBe(1);
-      });
+        ).toBe(1)
+      })
 
       it('should give correct decades for 365 days', () => {
         expect(
           whatis(365)
             .days()
             .in.decades(),
-        ).toBe(0.1);
-      });
-    });
+        ).toBe(0.1)
+      })
+    })
 
     describe('months', () => {
       it('should give correct seconds for 1 month', () => {
@@ -199,49 +202,49 @@ describe('Whatis', () => {
           whatis(1)
             .months()
             .in.seconds(),
-        ).toBe(2628000);
-      });
+        ).toBe(2628000)
+      })
 
       it('should give correct hours for 1 month', () => {
         expect(
           whatis(1)
             .months()
             .in.hours(),
-        ).toBe(730);
-      });
+        ).toBe(730)
+      })
 
       it('should give correct days for 1 month', () => {
         expect(
           whatis(1)
             .months()
             .in.days(),
-        ).toBe(30.416666666666668);
-      });
+        ).toBe(30.416666666666668)
+      })
 
       it('should give correct months for 12 months', () => {
         expect(
           whatis(12)
             .months()
             .in.months(),
-        ).toBe(12);
-      });
+        ).toBe(12)
+      })
 
       it('should give correct years for 12 months', () => {
         expect(
           whatis(12)
             .months()
             .in.years(),
-        ).toBe(1);
-      });
+        ).toBe(1)
+      })
 
       it('should give correct decades for 12 months', () => {
         expect(
           whatis(12)
             .months()
             .in.decades(),
-        ).toBe(0.1);
-      });
-    });
+        ).toBe(0.1)
+      })
+    })
 
     describe('years', () => {
       it('should give correct seconds for 1 year', () => {
@@ -249,49 +252,49 @@ describe('Whatis', () => {
           whatis(1)
             .years()
             .in.seconds(),
-        ).toBe(31536000);
-      });
+        ).toBe(31536000)
+      })
 
       it('should give correct hours for 1 year', () => {
         expect(
           whatis(1)
             .years()
             .in.hours(),
-        ).toBe(8760);
-      });
+        ).toBe(8760)
+      })
 
       it('should give correct days for 1 year', () => {
         expect(
           whatis(1)
             .years()
             .in.days(),
-        ).toBe(365);
-      });
+        ).toBe(365)
+      })
 
       it('should give correct months for 1 year', () => {
         expect(
           whatis(1)
             .years()
             .in.months(),
-        ).toBe(12);
-      });
+        ).toBe(12)
+      })
 
       it('should give correct years for 1 year', () => {
         expect(
           whatis(1)
             .years()
             .in.years(),
-        ).toBe(1);
-      });
+        ).toBe(1)
+      })
 
       it('should give correct decades for 1 year', () => {
         expect(
           whatis(1)
             .years()
             .in.decades(),
-        ).toBe(0.1);
-      });
-    });
+        ).toBe(0.1)
+      })
+    })
 
     describe('decades', () => {
       it('should give correct seconds for 1 decade', () => {
@@ -299,57 +302,57 @@ describe('Whatis', () => {
           whatis(1)
             .decades()
             .in.seconds(),
-        ).toBe(315360000);
-      });
+        ).toBe(315360000)
+      })
 
       it('should give correct hours for 1 decade', () => {
         expect(
           whatis(1)
             .decades()
             .in.hours(),
-        ).toBe(87600);
-      });
+        ).toBe(87600)
+      })
 
       it('should give correct days for 1 decade', () => {
         expect(
           whatis(1)
             .decades()
             .in.days(),
-        ).toBe(3650);
-      });
+        ).toBe(3650)
+      })
 
       it('should give correct months for 1 decade', () => {
         expect(
           whatis(1)
             .decades()
             .in.months(),
-        ).toBe(120);
-      });
+        ).toBe(120)
+      })
 
       it('should give correct year for 1 decade', () => {
         expect(
           whatis(1)
             .decades()
             .in.years(),
-        ).toBe(10);
-      });
+        ).toBe(10)
+      })
 
       it('should give correct decades for 1 decade', () => {
         expect(
           whatis(1)
             .decades()
             .in.decades(),
-        ).toBe(1);
-      });
-    });
-  });
+        ).toBe(1)
+      })
+    })
+  })
 
   describe('weight units', () => {
     describe('methods', () => {
-      const methods = ['gram', 'grams', 'kilogram', 'kilograms'];
+      const methods = ['gram', 'grams', 'kilogram', 'kilograms']
 
-      shouldHaveMethods(methods);
-    });
+      shouldHaveMethods(methods)
+    })
 
     describe('grams', () => {
       it('should give correct grams for 1 gram', () => {
@@ -357,17 +360,17 @@ describe('Whatis', () => {
           whatis(1)
             .grams()
             .in.grams(),
-        ).toBe(1);
-      });
+        ).toBe(1)
+      })
 
       it('should give correct kilograms for 1000 grams', () => {
         expect(
           whatis(1000)
             .grams()
             .in.kilograms(),
-        ).toBe(1);
-      });
-    });
+        ).toBe(1)
+      })
+    })
 
     describe('kilograms', () => {
       it('should give correct grams for 1 kilogram', () => {
@@ -375,16 +378,16 @@ describe('Whatis', () => {
           whatis(1)
             .kilograms()
             .in.grams(),
-        ).toBe(1000);
-      });
+        ).toBe(1000)
+      })
 
       it('should give correct kilograms for 1 kilogram', () => {
         expect(
           whatis(1)
             .kilograms()
             .in.kilograms(),
-        ).toBe(1);
-      });
-    });
-  });
-});
+        ).toBe(1)
+      })
+    })
+  })
+})
